95a6721377e5ea6ba94748ecb84db2fc3374dc2b
Cesar Tejaxun <ct@fibbo.app>
2021-06-20 15:20:37 -0600

    systemd - proyecto extracion de requirements y conexion a mongodb

--- proyecto/systemd/app.py
+++ proyecto/systemd/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = {
+    "db": "lab-bases2", "host": "104.248.234.39", "port": 27017,
+}
+db = MongoEngine()
+db.init_app(app)
+
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.EmailField()
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Wordddddld!</p>"
+    # User(name="bruh", email="cesar.etx@gmail.com").save()
+    users = User.objects.all()
+    return render_template("index.html", users=users)
