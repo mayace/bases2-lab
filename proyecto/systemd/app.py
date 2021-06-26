5450654ce29fb8132deff19d06cc5a6dba925c42
Cesar Tejaxun <ct@fibbo.app>
2021-06-25 20:43:55 -0600

    proyect - systemd - uso de settings con archivo .env

--- proyecto/systemd/app.py
+++ proyecto/systemd/app.py
@@ -1,11 +1,11 @@
+from settings import SQLS_CONN_STR
 from models import Movie
-from settings import MONGODB, NETFLIX_CONNECTION_STR
 from helpers.check_updates import Handler
 from flask import Flask, render_template
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = MONGODB
+app.config.from_pyfile("settings.py")
 db = MongoEngine()
 db.init_app(app)
 
@@ -14,12 +14,14 @@ db.init_app(app)
 def check_updates():
     first = Movie.objects.all().order_by("-created_at").first()
 
-    handler = Handler(NETFLIX_CONNECTION_STR)
+    handler = Handler(SQLS_CONN_STR)
     data = handler.execute(last_update=first.created_at if first else None)
     return render_template("last_updates.html", data=data)
 
 
 @app.route("/")
 def index():
+    movies = []
+    print(app.config.get("MONGODB"))
     movies = Movie.objects.all().order_by("-created_at")
     return render_template("index.html", movies=movies)
--- proyecto/systemd/settings.py
+++ proyecto/systemd/settings.py
@@ -1,7 +1,20 @@
 from os import environ
 
-MONGODB = {
-    "db": "lab-bases2", "host": "104.248.234.39", "port": 27017,
+SQLS_DRIVER = environ.get("SQLS_DRIVER", "SQL Server")
+SQLS_HOST = environ.get("SQLS_HOST", "localhost\\SQLEXPRESS")
+SQLS_DB = environ.get("SQLS_DB")
+SQLS_USER = environ.get("SQLS_USER")
+SQLS_PASS = environ.get("SQLS_PASS")
+
+
+MONGO_HOST = environ.get("MONGO_HOST")
+MONGO_PORT = environ.get("MONGO_PORT", 27017)
+MONGO_DB = environ.get("MONGO_DB")
+
+
+MONGODB_SETTINGS = {
+    "db": MONGO_DB, "host": MONGO_HOST, "port": int(MONGO_PORT),
 }
 
-NETFLIX_CONNECTION_STR = "Driver={SQL Server};Server=.\SQLEXPRESS;Database=PruebaTecnica;Trusted_Connection=yes;"
+SQLS_CONN_STR = "Driver={};Server={};Database={};UID={};PWD={};".format(SQLS_DRIVER,
+                                                                        SQLS_HOST, SQLS_DB, SQLS_USER, SQLS_PASS,)
