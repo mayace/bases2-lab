2d9bbc27787ebf9bd58158f662268df38f0498fe
Cesar Tejaxun <ct@fibbo.app>
2021-06-26 22:16:28 -0600

    proyecto - systemd - flask actualizadion de modelos para movie, netflix update, episode y genre como tambien popular desde sql

--- proyecto/systemd/helpers/check_updates.py
+++ proyecto/systemd/helpers/check_updates.py
@@ -1,6 +1,7 @@
+import datetime
 from flask.app import Flask
 import pyodbc
-from models import Movie
+from models import Episode, Genre, Movie, NetflixUpdate
 
 
 class Handler():
@@ -10,15 +11,70 @@ class Handler():
     def create_sqlserver_connection(self):
         return pyodbc.connect(self.sql_connection_str)
 
+    def get_fields(self, item):
+        fields = dict()
+        fields["imdb_rating"] = getattr(item, "averageRating", None)
+        fields["title"] = getattr(item, "primaryTitle", None)
+        fields["netflix_id"] = getattr(item, "id", None)
+        fields["year"] = getattr(item, "startYear", None)
+        fields["created_at"] = getattr(item, "createdAt", None)
+        return fields
+
+    def get_genres_query(self) -> str:
+        return """
+        select g.id, g.name
+        from TitleGenre tg
+        left join Genre g on g.id = tg.genreId
+        where titleId = ?
+        """
+
+    def get_episodes_query(self) -> str:
+        return """
+        select g.id, g.name
+        from TitleGenre tg
+        left join Genre g on g.id = tg.genreId
+        where titleId = ?
+        """
+
+    def append_genres(self, conn, movie: Movie):
+        conn = self.create_sqlserver_connection()
+        cursor = conn.cursor()
+        query = self.get_genres_query()
+        for item in cursor.execute(query, movie.netflix_id):
+            genre = Genre()
+            genre.name = getattr(item, "name", None)
+            movie.genres.append(genre)
+
+    def append_episodes(self, conn, movie: Movie):
+        conn = self.create_sqlserver_connection()
+        cursor = conn.cursor()
+        query = self.get_episodes_query()
+        for item in cursor.execute(query, movie.netflix_id):
+            doc = Episode()
+            doc.name = getattr(item, "name", None)
+            doc.number = getattr(item, "number", None)
+            movie.episodes.append(doc)
+
     def get_updates(self, last_update) -> list:
         conn = self.create_sqlserver_connection()
         cursor = conn.cursor()
-        l = list()
+        new_list = list()
 
-        for item in cursor.execute("execute get_updates ?;", last_update):
-            l.append(Movie(title=item.title, imdb_rating=item.imdb_rating,
-                     netflix_id=item.netflix_id, year=item.year, created_at=item.created_at).save())
-        return l
+        this_update = NetflixUpdate().save()
+        for item in cursor.execute("execute select_news_titles ?;", last_update or ""):
+            fields = self.get_fields(item)
+            if fields.get("netflix_id"):
+                movie = Movie(netflix_update=this_update, **fields)
+                self.append_episodes(conn, movie)
+                self.append_genres(conn, movie)
+                movie.save()
+                new_list.append(movie)
+
+        this_update.total = len(new_list)
+        this_update.end_date = datetime.datetime.utcnow
+        this_update.save()
+
+        return new_list
 
     def execute(self, last_update=None) -> list:
         return self.get_updates(last_update)
--- proyecto/systemd/models.py
+++ proyecto/systemd/models.py
@@ -1,6 +1,23 @@
 from mongoengine import Document, StringField
 from mongoengine.document import EmbeddedDocument
-from mongoengine.fields import DateTimeField, FloatField, IntField, ListField
+from mongoengine.fields import DateTimeField, EmbeddedDocumentField, FloatField, IntField, ListField, ReferenceField
+import datetime
+
+
+class NetflixUpdate(Document):
+    start_date_param = DateTimeField()
+    total = IntField()
+    start_date = DateTimeField(default=datetime.datetime.utcnow)
+    end_date  = DateTimeField()
+
+
+class Episode (EmbeddedDocument):
+    name = StringField()
+    number = IntField()
+
+
+class Genre(EmbeddedDocument):
+    name = StringField()
 
 
 class Movie(Document):
@@ -8,10 +25,12 @@ class Movie(Document):
     title = StringField()
     imdb_rating = FloatField()
     year = IntField()
-    episodes = ListField(StringField(), default=list) # solo total
-
     created_at = DateTimeField()
-    # genres = ListField(StringField(), default=list) # solo total
+
+    episodes = ListField(EmbeddedDocumentField(Episode), default=list)
+    genres = ListField(EmbeddedDocumentField(Genre), default=list)
+
+    netflix_update = ReferenceField(NetflixUpdate)
 
     # start_year = IntField()
     # end_year = IntField()
